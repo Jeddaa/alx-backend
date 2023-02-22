c6b5773be8672677717b3cb6cb582a005c1788fd
jeddaa <fathiato@gmail.com>
2023-02-22 14:56:33 +0100

    lru cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" LRU caching module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """class LRU cache"""
+
+    def __init__(self):
+        """constructor function"""
+        super().__init__()
+        self.cache_data = OrderedDict(self.cache_data)
+
+    def put(self, key, item):
+        """function to put the items"""
+        new_dict = self.cache_data
+        dictlen = len(self.cache_data)
+        if key is None or item is None:
+            pass
+        else:
+            if dictlen >= BaseCaching.MAX_ITEMS:
+                last = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {last[0]}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """function to get"""
+        if key is None and key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
