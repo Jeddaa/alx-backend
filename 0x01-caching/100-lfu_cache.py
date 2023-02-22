471f201dd62796e8bb5996c4ab6ef36e576f06af
jeddaa <fathiato@gmail.com>
2023-02-22 17:30:07 +0100

    lfu cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,7 +10,6 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """constructor function"""
         super().__init__()
-        # self.used = []
         self.index = {}
 
     def put(self, key, item):
@@ -24,11 +23,6 @@ class LFUCache(BaseCaching):
                 print(f"DISCARD: {min_key}")
                 del self.cache_data[min_key]
                 del self.index[min_key]
-                # self.used.remove(min_key)
-                # del self.used[0]
-            # if key in self.used:
-            #     del self.used[self.used.index(key)]
-            # self.used.append(key)
             self.index[key] = self.index.get(key, 0) + 1
             self.cache_data[key] = item
 
@@ -36,7 +30,5 @@ class LFUCache(BaseCaching):
         """function to get"""
         if key is not None and key in self.cache_data.keys():
             self.index[key] = self.index.get(key, 0) + 1
-            # del self.used[self.used.index(key)]
-            # self.used.append(key)
             return self.cache_data.get(key)
         return None
