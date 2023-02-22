7687a56060e2e1eb6da1398b3fd16e726044ec7b
jeddaa <fathiato@gmail.com>
2023-02-22 01:33:43 +0100

    fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''task 1'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''class fifo cache'''
+
+    def __init__(self):
+        '''constructor function'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''function to put the items'''
+        dictlen = len(self.cache_data)
+        if key is None or item is None:
+            pass
+        else:
+            if dictlen > BaseCaching.MAX_ITEMS:
+                # self.cache_data[self.order]
+                first = next(iter(self.cache_data))
+                self.cache_data.pop(first)
+                print(f"DISCARD: {first}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''function to get '''
+        if key is None and key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
