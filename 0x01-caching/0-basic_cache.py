c9b6b59ef9718f320262880df9372b1da771f4b9
jeddaa <fathiato@gmail.com>
2023-02-22 01:56:41 +0100

    fifo cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,40 +1,25 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
+""" task 0 """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    Defines a class for caching information in key-value pairs
-    Methods:
-        put(key, item) - store a key-value pair
-        get(key) - retrieve the value associated with a key
-    """
+    """ basic cache class """
 
     def __init__(self):
-        """
-        Initialize the class using the parent class __init__ method
-        """
-        BaseCaching.__init__(self)
+        """initialization function """
+        super().__init__()
 
     def put(self, key, item):
-        """
-        Store a key-value pair
-        Args:
-            Key
-            Item
-        """
+        """function to pass item value to dictionary"""
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Return value linked to key.
-        If key is None or doesn't exist, return None
-        """
-        if key is not None and key in self.cache_data.keys():
-            return self.cache_data[key]
-        return None
+        """ function to get """
+        if key is None and key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
