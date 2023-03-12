ac41fbf3338a6ddbf06fffd9ded42b9af1950bce
jeddaa <fathiato@gmail.com>
2023-03-12 02:20:29 +0100

    finally

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ''' Basic flask application '''
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 
 class Config(object):
@@ -18,20 +18,19 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    '''first route '''
-    return render_template('3-index.html')
-
-
 # comment line 28 and uncomment line 34 for this to work as
 # the new babel doen't support @babel.localselector anymore
 # this was left this way because of alx checker
 @babel.localeselector
 def get_locale():
     ''' get locale from request header '''
-    # return 'fr'
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''first route '''
+    return render_template('3-index.html')
 
 
 # babel.init_app(app, locale_selector=get_locale)
